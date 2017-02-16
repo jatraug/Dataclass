aca71abfe85cb10fd12fc626d2a41815eb6bd791
Jim Traugott <jtraug@gmail.com>
2017-02-16 15:04:25 -0800

    Complete

--- Module3/assignment2.py
+++ Module3/assignment2.py
@@ -12,30 +12,39 @@ plt.style.use('ggplot')
 # It's located at 'Datasets/wheat.data'
 # 
 # .. your code here ..
-
+df = pd.read_csv('Datasets/wheat.data', sep=',')
+print(df.head())
 
 #
 # TODO: Create a 2d scatter plot that graphs the
 # area and perimeter features
 # 
 # .. your code here ..
-
-
+s1 = df.loc[:,'area':'perimeter']
+print('s1:')
+print(s1.head())
+s1.plot.scatter('area', 'perimeter' )
 #
 # TODO: Create a 2d scatter plot that graphs the
 # groove and asymmetry features
 # 
 # .. your code here ..
-
-
+s2 = df.loc[:,'asymmetry':'groove']
+print('s2:')
+print(s2.head())
+s2.plot.scatter('asymmetry','groove')
 #
 # TODO: Create a 2d scatter plot that graphs the
 # compactness and width features
 # 
 # .. your code here ..
+s3 = df.loc[:,'compactness':'width']
+print('s3:')
+print(s3.head())
+s3.plot.scatter('compactness','width')
+#
 
-
-
+plt.show()
 # BONUS TODO:
 # After completing the above, go ahead and run your program
 # Check out the results, and see what happens when you add
