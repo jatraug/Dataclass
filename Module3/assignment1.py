76e57f11d7b7ed719c30ab5d7ca1f38c3680a159
Jim Traugott <jtraug@gmail.com>
2017-02-16 15:05:11 -0800

    Complete

--- Module3/assignment1.py
+++ Module3/assignment1.py
@@ -12,21 +12,24 @@ plt.style.use('ggplot')
 # It's located at 'Datasets/wheat.data'
 # 
 # .. your code here ..
-
-
+df = pd.read_csv('Datasets/wheat.data', sep=',')
+print(df.head())
 #
 # TODO: Create a slice of your dataframe (call it s1)
 # that only includes the 'area' and 'perimeter' features
 # 
 # .. your code here ..
-
-
+s1 = df.loc[:,'area':'perimeter']
+print('s1:')
+print(s1.head())
 #
 # TODO: Create another slice of your dataframe (call it s2)
 # that only includes the 'groove' and 'asymmetry' features
 # 
 # .. your code here ..
-
+s2 = df.loc[:,'asymmetry':'groove']
+print('s2:')
+print(s2.head())
 
 #
 # TODO: Create a histogram plot using the first slice,
@@ -34,6 +37,8 @@ plt.style.use('ggplot')
 # Be sure to set alpha=0.75
 # 
 # .. your code here ..
+s1.hist( alpha=0.75, bins=30)
+s2.hist( alpha=0.75, bins=30)
 
 
 # Display the graphs:
