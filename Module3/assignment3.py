29b04f487c3be533216bf100cb5dee74bf312748
Jim Traugott <jtraug@gmail.com>
2017-03-09 16:13:37 -0800

    finished

--- Module3/assignment3.py
+++ Module3/assignment3.py
@@ -14,9 +14,14 @@ plt.style.use('ggplot')
 # 
 # .. your code here ..
 
-
+df = pd.read_csv('Datasets/wheat.data', sep=',')
+print(df.head())
 
 fig = plt.figure()
+ax = Axes3D(fig)
+print('area')
+print(df.loc[:,'area'])
+
 
 #
 # TODO: Create a new 3D subplot using fig. Then use the
@@ -27,8 +32,14 @@ fig = plt.figure()
 # 
 # .. your code here ..
 
+ax.scatter(df.loc[:,'area'], df.loc[:,'perimeter'], df.loc[:,'asymmetry'], c='red')
+ax.set_xlabel('area')
+ax.set_ylabel('perimeter')
+ax.set_zlabel('asymmetry')
+
 
 fig = plt.figure()
+ax2 = Axes3D(fig)
 #
 # TODO: Create a new 3D subplot using fig. Then use the
 # subplot to graph a 3D scatter plot using the width,
@@ -37,6 +48,10 @@ fig = plt.figure()
 # axes
 # 
 # .. your code here ..
+ax2.scatter(df.loc[:,'width'], df.loc[:,'groove'], df.loc[:,'length'], c='green')
+ax2.set_xlabel('width')
+ax2.set_ylabel('groove')
+ax2.set_zlabel('length')
 
 
 plt.show()
